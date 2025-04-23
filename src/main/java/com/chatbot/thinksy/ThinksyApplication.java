b26ebabea06699c6505a75b7ce3918c7bddadf5c
subash770 <spidybeatz25@gmail.com>
2025-04-23 12:37:12 +0530

    chatbot-up-1

---
+++ src/main/java/com/chatbot/thinksy/ThinksyApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.thinksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinksyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinksyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatbot/thinksy/ThinksyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.thinksy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinksyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
